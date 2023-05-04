b85b14f2af298e389c958015e1eff964a176fd87
André Carlos <andresinho200498@gmail.com>
2023-05-04 02:22:09 -0300

    Test Webhook telegram api

--- src/pages/api/bot/index.ts
+++ src/pages/api/bot/index.ts
@@ -1,8 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Update } from "node-telegram-bot-api";
 
 const handlerBotWebHook = async (request: NextApiRequest, response: NextApiResponse) => {
 
-    console.log(request.body);
+    const update:Update = request.body;
+    console.log(`#### Start Request #####`);
+    console.log(`Update: ${update.update_id}`);
+    console.log(JSON.stringify(update));
+    console.log(`### Finish Request #####`);
 
     response.status(200).json({message: 'OK'});
 }
